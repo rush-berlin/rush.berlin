3d4b3a7ba44061b0a251660021a32f6ca016625a
Rain <rain@synth.kitchen>
2024-12-08 11:52:04 +0100

    delete /podcast

--- podcast/scripts/load-rss.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-const load = (RSS_URL) =>
-  fetch(RSS_URL)
-    .then((response) => response.text())
-    .then((xml) => new window.DOMParser().parseFromString(xml, "text/xml"))
-    .then((data) => Array.from(data.getElementsByTagName("item")));
